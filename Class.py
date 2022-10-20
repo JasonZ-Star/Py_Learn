ea59f6fea8ead97203453f2ea5c94568422005de
Jason_Z <976342383@qq.com>
2022-10-20 22:44:51 +0800

    version 1

--- Class.py
+++ Class.py
@@ -11,6 +11,21 @@ class Students:
     def say_hi(self):
         print(f"Hi，大家好，我是{self.name}")
 
+    # str 魔术方法
+    def __str__(self):
+        print(f'我是{self.name},我的年龄是{self.age}')
+
+    # lt魔术方法
+    def __lt__(self, other):
+        return self.age < other.age  # 不能写小于等于
+
+    def __le__(self, other):
+        return self.age <= other.age  # 可以写小于等于
+
+    # eq魔术方法，比较两个对象是否相等
+    def __eq__(self, other):
+        return self.age == other.age
+
 
 # 创建对象
 stu_1 = Students()
@@ -18,4 +33,7 @@ stu_1.name = "王"
 stu_1.age = 18
 stu_1.school = "武隆中学"
 
-
+stu_2 = Students()
+stu_2.name = "朱"
+stu_2.age = 20
+stu_2.school = "白塔中学"
